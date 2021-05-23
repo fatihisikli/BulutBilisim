9758719401c2e5c7ad6ee76530bbc55dd1173a34
fatihisikli <root.fatih@gmail.com>
2021-05-23 15:56:46 +0300

    asd

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +28,8 @@ namespace BulutBilisim
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<BulutBilisim.Models.BulutBilisimContext>();
+            services.AddDbContext<Models.BulutBilisimContext>(options =>
+        options.UseSqlServer(Configuration.GetConnectionString("BulutBilisimContext")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
